3a24f88c96c69bec183207c4ff0a2dcba29bddc5
Hovhannes <hovhannes.mkoyan@gmail.com>
2022-01-10 13:20:24 +0400

    finalizing the code

--- udp.js
+++ udp.js
@@ -1,8 +1,19 @@
 const dgram = require('dgram');
+
 const socket = dgram.createSocket('udp4');
+
+socket.on('error', (err) => {
+    console.log(`server error:\n${err.stack}`);
+    socket.close();
+});
 
 socket.on('message', (msg, rinfo) => {
     console.log(`server got: ${msg} from ${rinfo.address}:${rinfo.port}`);
+});
+
+socket.on('listening', () => {
+    const address = socket.address();
+    console.log(`server listening ${address.address}:${address.port}`);
 });
 
 socket.bind(7000);
